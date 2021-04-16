b62d88812937c4a6e9264943b5598aad3e674925
M.Tamura <tamura2004@gmail.com>
2021-04-16 22:10:33 +0900

    Add server usres

--- discordbot.rb
+++ discordbot.rb
@@ -6007,7 +6007,13 @@ bot = Discordrb::Bot.new token: TOKEN
 
 bot.message(start_with: "!game") do |event|
   magic = rand(1..10)
-  event.respond "hello #{event.user}. Can you guess my secret number? between 1 and 10"
+  users = ""
+  bot.servers.each_value do |srv|
+    srv.users.each do |user|
+      users += user.name
+    end
+  end
+  event.respond "hello #{users}. Can you guess my secret number? between 1 and 10"
   event.user.await!(timeout: 300) do |guess_event|
     guess = guess_event.message.content.to_i
     if guess == magic

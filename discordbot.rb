529036c2d656b7e53bc45052973e99214fd1f534
M.Tamura <tamura2004@gmail.com>
2021-06-06 22:28:21 +0900

    Update dungeon

--- discordbot.rb
+++ discordbot.rb
@@ -1,9 +1,12 @@
 require "discordrb"
 require "yaml"
+require "./dummybot.rb"
+require "./rolldice.rb"
 
 TOKEN = ENV["DISCORD_BOT_TOKEN"]
 TOKEN.freeze
 bot = Discordrb::Bot.new token: TOKEN
+# bot = DummyBot::Bot.new
 
 meros = YAML.load(open("meros.yaml").read)
 rodger = YAML.load(open("rodger.yaml").read)
@@ -46,6 +49,8 @@ end
 
 # 格言ボット
 bot.message(contains: /[るだす？]$/) do |event|
+  next if event.channel.name == "狂王の祭祀場" && event.channel.name == "ボットデバッグ用"
+
   if rand < 0.5
     k = kei
     event.respond("#{k}#{mei}より#{k}#{mei}。")
@@ -57,22 +62,25 @@ end
 
 # あいさつボット
 bot.message(contains: /おは|こん|おや|堕|ぐっど|ただいま|てら|ちち/) do |event|
-  pp event.channel.name
+  next if event.channel.name == "狂王の祭祀場" && event.channel.name == "ボットデバッグ用"
   event.respond(greet)
 end
 
 # ロジャーボット
 bot.message(contains: /ろじゃー|だんじょん|ばーぐる/) do |event|
+  next if event.channel.name == "狂王の祭祀場" && event.channel.name == "ボットデバッグ用"
   event.respond say(rodger, nil, nil, 3)
 end
 
 # メロスボット
 bot.message(contains: /めろす|はしれ/) do |event|
+  next if event.channel.name == "狂王の祭祀場" && event.channel.name == "ボットデバッグ用"
   event.respond say(meros, nil, nil, 3)
 end
 
 # 何の日ボット
 bot.message(contains: /月|日|時|分|秒/) do |event|
+  next if event.channel.name == "狂王の祭祀場" && event.channel.name == "ボットデバッグ用"
   event.respond say(nanohi, nil, "年", 1)
 end
 
@@ -81,26 +89,14 @@ bot.message(contains: /\:.*d.*\:/) do |event|
   event.respond rolldice(event.content)
 end
 
-include Discordrb::Webhooks
-
-bot.reaction_add do |event|
-  pp event
-end
-
 class Player
-  attr_accessor :name
-  attr_accessor :race
-  attr_accessor :klass
-  attr_accessor :lv
-  attr_accessor :rpw
-  attr_accessor :pw
-  attr_accessor :rhp
-  attr_accessor :hp
-  attr_accessor :sp
+  attr_accessor :name, :race, :klass, :lv, :rpw, :pw, :rhp, :hp, :sp, :place, :gp, :exp
 
   def initialize(id)
     @id = id
     @lv = 1
+    @gp = 10
+    @exp = 0
   end
 
   def set_race(data)
@@ -116,39 +112,61 @@ class Player
     @sp = sp
   end
 
-  def levelup
-    @lv += 1
+  def levelup(e)
+    @exp += e
+    if @lv * 10 <= @exp
+      @lv += 1
+      @pw = lv + rpw
+      @hp = lv + rhp
+      "#{name}はレベルアップ！#{klass} #{to_s}"
+    else
+      "#{name}は#{e}経験値を得た。"
+    end
+  end
+
+  def raisefromdead
     @pw = lv + rpw
     @hp = lv + rhp
-    "#{name}はレベルアップ！#{klass} #{status}"
+    @gp -= lv
+    gp >= 0
   end
 
   def to_s
-    "#{name}:#{race}の#{klass}#{lv} #{status}"
-  end
-
-  def status
-    "#{pw}/#{hp}"
+    "#{name}(#{race}の#{klass}#{lv}lv #{pw}/#{hp} #{gp}gp #{exp}xp)"
   end
 end
 
-def d3
-  rand(3) - 1
+def d(n)
+  rand(n) - (n / 2)
 end
 
 class Monster
-  attr_accessor :name, :lv, :pw, :hp
+  attr_accessor :name, :lv, :pw, :hp, :cdm
+  RANK = ["", "チーフ", "リーダー", "キング", "エンペラー", "ゴッド"]
   NAME = %w(ゴブリン オーク バグベア スケルトン ミノタウルス ゴーレム ドラゴン)
 
-  def initialize
-    @lv = rand(7)
-    @name = NAME[lv]
-    @pw = lv + d3
-    @hp = lv + d3
+  def initialize(base_lv)
+    @lv = base_lv + rand(3)
+    @name = NAME[lv % 7] + RANK[lv / 7]
+    @pw = lv + d(3)
+    @hp = lv + d(3)
+    @cdm = Hash.new(0)
+  end
+
+  def get_damage(dm, pc)
+    cdm[pc] = dm if cdm[pc] < dm
+  end
+
+  def damage
+    cdm.values.sum
+  end
+
+  def dead?
+    hp <= damage
   end
 
   def to_s
-    "#{name} 攻#{pw}/防#{hp}"
+    "#{name} 攻#{pw}/防#{hp - damage}"
   end
 end
 
@@ -166,6 +184,69 @@ klasses = [
   [/せん|戦士/, "戦士", "剣盾"],
 ]
 
+def select_command(rs, a)
+  rs.index { |r| r =~ a } || rand(rs.size)
+end
+
+class Menu
+  attr_accessor :yomi, :label
+
+  def initialize(yomi, label)
+    @yomi = yomi
+    @label = label
+  end
+end
+
+class Menues
+  attr_accessor :menues
+
+  def initialize(items)
+    @menues = items.map do |item|
+      Menu.new(*item)
+    end
+  end
+
+  def <<(menu)
+    menues << menu
+  end
+
+  def message
+    msg = []
+    menues.each_with_index do |menu, i|
+      msg << "#{i + 1}.[#{menu.yomi}]#{menu.label}"
+    end
+    msg.join(",")
+  end
+
+  def select(s)
+    case s
+    when /[1-9]/
+      i = s.to_i - 1
+      (menues[i] || menues.sample).label
+    else
+      m = menues.find { |m| s =~ /#{m.yomi}/ || s =~ /#{m.label}/ }
+      m ||= menues.sample
+      m.label
+    end
+  end
+end
+
+town_menues = Menues.new([
+  ["お", "王城"],
+  ["ぶ", "武器屋"],
+  ["ぼ", "防具屋"],
+  ["だ", "ダンジョン"],
+])
+
+dungeon_menues = Menues.new(
+  [
+    ["た", "戦う"],
+    ["さ", "探す"],
+    ["す", "進む"],
+    ["に", "逃げる"],
+  ]
+)
+
 bot.message do |event|
   next if event.channel.name != "狂王の祭祀場" && event.channel.name != "ボットデバッグ用"
 
@@ -177,12 +258,11 @@ bot.message do |event|
   case
   when pc.nil? || text =~ /りせっと/
     players[id] = Player.new(id)
-    event.respond("#{auther}さんのキャラクターネームは？")
+    event << "#{auther}さんのキャラクターネームは？"
   when pc.name.nil?
     name = event.content
     pc.name = name
-    event << "#{auther}さんのキャラクターは、#{name}さんです。"
-    event << "種族は？"
+    event << "#{auther}さんのキャラクターは、#{name}さんです。種族は？"
   when pc.race.nil?
     race = races.find do |r|
       event.content =~ r[0]
@@ -194,28 +274,89 @@ bot.message do |event|
       event.content =~ c[0]
     end || klasses[-1]
     pc.set_klass(klass)
-    event << "#{pc.name}:#{pc.race}の#{pc.klass} 攻#{pc.pw}/防#{pc.hp} #{pc.sp}"
-  when text =~ /たた/
-    monsters << Monster.new if monsters.empty?
-    m = monsters[0]
-    dm = pc.pw + d3
-    event << "#{pc.name}は#{m.to_s}に攻撃。#{dm}ダメージ。"
-    m.hp -= dm
-    if m.hp <= 0
-      event << "#{m.to_s}は死んだ。"
-      monsters.shift
-      event << pc.levelup
-    else
-      dm = m.pw + d3
-      event << "#{m.to_s}の反撃。#{dm}ダメージ。"
-      pc.hp -= dm
-      if pc.hp <= 0
-        pc.pw += 1
-        pc.hp = 1
-        event << "#{pc.name}は地面に倒れソウルを失った。攻#{pc.pw}/防#{pc.hp}"
+    pc.place = "リルガミン"
+  when pc.place == "リルガミン"
+    case town_menues.select(event.content)
+    when /王城/
+      event << "#{pc.name}は王城に行った。王様「支度金である」"
+      pc.gp += pc.lv
+      pc.exp -= pc.lv
+    when /武器/
+      if pc.gp >= pc.lv
+        event << "#{pc.name}は武器屋に行った。折れた直剣を#{pc.lv}gpで買った。"
+        pc.gp -= pc.lv
+        pc.pw += pc.lv
       else
+        event << "#{pc.name}は武器屋に行ったが所持金が足りない。"
       end
+    when /防具/
+      if pc.gp >= pc.lv
+        event << "#{pc.name}は防具屋に行った。汚れた鎧を#{pc.lv}gpで買った。"
+        pc.gp -= pc.lv
+        pc.hp += pc.lv
+      else
+        event << "#{pc.name}は防具屋に行ったが所持金が足りない。"
+      end
+    when /ダンジョン/
+      event << "#{pc.name}はダンジョンに入った"
+      pc.place = "ダンジョン"
     end
+  when pc.place == "ダンジョン"
+    case dungeon_menues.select(event.content)
+    when /戦う/
+      m = monsters[0]
+      dm = pc.pw + d(5)
+      event << "#{pc.name}は#{m.to_s}に攻撃。#{dm}ダメージ。"
+      m.get_damage(dm, id)
+      if m.dead?
+        event << "#{m.to_s}は死んだ。"
+        monsters.shift
+        event << pc.levelup(m.lv)
+      else
+        dm = m.pw + d(5)
+        event << "#{m.to_s}の反撃。#{dm}ダメージ。"
+        pc.hp -= dm
+        if pc.hp <= 0
+          pc.place = "カント寺院"
+        else
+        end
+      end
+    when /探す/
+      if rand(6) < 3
+        g = rand(1..10) * pc.lv
+        pc.gp += g
+        event << "#{g}gp見つけた。"
+      else
+        event << "なにも見つからなかった。"
+      end
+    when /進む/
+      event << "#{pc.name}は奥に進む。"
+    when /逃げる/
+      event << "#{pc.name}は逃げ出した。"
+      pc.place = "リルガミン"
+    end
+  end
+
+  next if pc.nil?
+
+  case pc.place
+  when "ダンジョン"
+    monsters << Monster.new(pc.lv) if monsters.empty?
+    event << monsters.first.to_s
+    event << "#{pc}はどうする？#{dungeon_menues.message}"
+  when "カント寺院"
+    if pc.raisefromdead
+      event << "#{pc.name}は死んだ。カント寺院で蘇生。#{pc.lv}gp寄付した。"
+      event << pc.to_s
+      pc.place = "リルガミン"
+    else
+      event << "#{pc.name}は死んだ。蘇生費用が無い。ロストしました。"
+      players.delete(id)
+    end
+  end
+
+  if pc.place == "リルガミン"
+    event << "#{pc}はどうする？#{town_menues.message}"
   end
 end
 
---
+++ dummybot.rb
@@ -0,0 +1,73 @@
+module DummyBot
+  class Listener
+    attr_accessor :filter, :block
+
+    def initialize(filter, block)
+      @filter = filter
+      @block = block
+    end
+  end
+
+  class Bot
+    attr_accessor :event, :listeners
+
+    def initialize
+      @event = Event.new
+      @listeners = []
+    end
+
+    def message(contains: //, &block)
+      listeners << Listener.new(contains, block)
+    end
+
+    def run
+      loop do
+        event.content = gets.chomp
+        event.msgs = []
+        listeners.each do |listener|
+          if event.content =~ listener.filter
+            listener.block.call(event)
+          end
+        end
+        event.msgs.each do |msg|
+          puts msg
+        end
+      end
+    end
+  end
+
+  class Author
+    attr_accessor :display_name, :id
+
+    def initialize
+      @id = 1
+      @display_name = "ななしさん"
+    end
+  end
+
+  class Channel
+    attr_accessor :name
+
+    def initialize
+      @name = "狂王の祭祀場"
+    end
+  end
+
+  class Event
+    attr_accessor :author, :content, :msgs, :channel
+
+    def initialize
+      @author = Author.new
+      @msgs = []
+      @channel = Channel.new
+    end
+
+    def <<(msg)
+      msgs << msg
+    end
+
+    def respond(msg)
+      msgs << msg
+    end
+  end
+end
